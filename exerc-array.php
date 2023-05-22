ec86cfcbc987eeb345de5c048d0c7dc9224ec976
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-22 17:45:37 -0300

    mb_strtoupper in name countries

--- exerc-array.php
+++ exerc-array.php
@@ -10,3 +10,9 @@ $dados = json_decode($json, true);
 $numeroDePaises = count($dados);
 
 echo 'Número de paises participantes = ' . $numeroDePaises . PHP_EOL;
+
+// Fazer com que seus nomes fiquem em letras maiúsculas
+
+foreach($dados as $nomePaises){
+    echo mb_strtoupper($nomePaises['pais']) . PHP_EOL;
+}
